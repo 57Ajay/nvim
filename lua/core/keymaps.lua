8f43c607050f553d7bda1f3928c1848d5c6acf82
57ajay <57ajay.u@gmail.com>
2025-09-10 10:17:03 +0530

    little change

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -101,3 +101,12 @@ vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true, desc = "Move current line up" })
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = "Move selection up" })
 
+-- Insert Go error check block
+keymap("n", "<leader>qq", function()
+  vim.api.nvim_put({
+    "if err != nil {",
+    '    log.Fatal("some error: ", err)',
+    "}",
+  }, "l", true, true)
+end, { desc = "Insert Go err check" })
+
